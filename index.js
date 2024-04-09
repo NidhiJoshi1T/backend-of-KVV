e5cfb1888e5a8cdeea14cb157eceadd3da956e91
Nidhi Joshi <nidhi2001kpg@gmail.com>
2024-04-09 13:25:47 +0530

    ,,

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/upload", upload.single('product'), (req, res)=>{
 
     res.json({
         success:1,
-        image_url:`${BASE_URL}/images/${req.file.filename}`
+        image_url:`${BASE_URL}images/${req.file.filename}`
     })
 })
 
